7a26a7d0945e8a7d06664a54800f80bb8224f540
patinas <andreas.patinas@gmail.com>
2022-02-24 09:06:37 +0100

    Update Remote_Desktop_Enable_Demo.ps1

--- Remote_Desktop_Enable_Demo.ps1
+++ Remote_Desktop_Enable_Demo.ps1
@@ -1,3 +1,8 @@
-﻿zerotier-cli join 9f77fc393e977c15
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+
+choco install zerotier-one
+zerotier-cli join 9f77fc393e977c16
+
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
