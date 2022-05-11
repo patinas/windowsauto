099c2ab2aa22ce70ae65302e2345c9157901d2fa
patinas <andreas.patinas@gmail.com>
2022-05-11 13:23:17 +0200

    Update ConvertToExe.ps1

--- ConvertToExe.ps1
+++ ConvertToExe.ps1
@@ -1,4 +1,4 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process -Force;
+Set-ExecutionPolicy Bypass -Scope Process -Force;
 Install-Module ps2exe
 Import-Module ps2exe
-Invoke-ps2exe "C:\Users\user\Downloads\Windows Deployment-20211220T121527Z-001\Windows Deployment\Remote_Desktop_Enable.ps1" "C:\Users\user\Downloads\Windows Deployment-20211220T121527Z-001\Windows Deployment\Remote_Desktop_Enable.exe"
+Invoke-ps2exe .\Remote_Desktop_Enable.ps1 .\Remote_Desktop_Enable.exe
