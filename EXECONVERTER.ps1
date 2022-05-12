ff8f1032086ffdd7437c02955827b4244673ea3b
patinas <andreas.patinas@gmail.com>
2022-05-12 11:27:22 +0200

    Add files via upload

---
+++ EXECONVERTER.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+Install-Module ps2exe
+Import-Module ps2exe
+Invoke-ps2exe .\WindowsAuto.ps1 .\WindowsAuto.exe
