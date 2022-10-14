0612f629127efe9bca1bf8bbb245084e2a396471
patinas <andreas.patinas@gmail.com>
2022-10-14 13:12:05 +0200

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -18,16 +18,3 @@ zerotier-cli join 9f77fc393e977c16
 
 
 
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco feature enable -n allowGlobalConfirmation
-
-choco install zerotier-one
-
-Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
-zerotier-cli join 9f77fc393e977c16
-
-
-
