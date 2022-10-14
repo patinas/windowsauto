7c316dabd8aea1e8c0baea7274dc92976a07c40a
patinas <andreas.patinas@gmail.com>
2022-10-14 14:01:02 +0200

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -4,7 +4,7 @@ choco install zerotier-one googlechrome teamviewer 7zip vlc adobereader spotify 
 
 choco feature enable -y allowGlobalConfirmation
 
-choco install zerotier-one
+choco install zerotier-one -y
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
