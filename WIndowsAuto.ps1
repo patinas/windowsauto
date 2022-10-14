33e651e8764e276457269d2cd4da4c0ff41e7ebe
patinas <andreas.patinas@gmail.com>
2022-10-14 13:10:19 +0200

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -3,7 +3,7 @@
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install onlyoffice zerotier-one googlechrome teamviewer 7zip vlc adobereader spotify winrar googledrive sharex
+choco install zerotier-one googlechrome teamviewer 7zip vlc adobereader spotify winrar googledrive sharex -y
 
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
