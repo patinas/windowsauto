6a452dbad521973c8f5b5a49f7d1c7463fb67587
patinas <andreas.patinas@gmail.com>
2022-10-14 13:13:20 +0200

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -1,6 +1,3 @@
-﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-
-
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install zerotier-one googlechrome teamviewer 7zip vlc adobereader spotify winrar googledrive sharex -y
