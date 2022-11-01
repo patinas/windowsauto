d15b7f54802d282c19a60ab45e8221f944251cee
patinas <andreas.patinas@gmail.com>
2022-11-01 22:30:55 +0100

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -1,7 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-cmd
-
 choco install zerotier-one googlechrome teamviewer 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey powertoys mailspring -y
 
 choco feature enable -y allowGlobalConfirmation
@@ -11,7 +9,13 @@ choco install zerotier-one -y
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-zerotier-cli join 9f77fc393e977c16
-
-
-
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+$destination = "C:\Program Files (x86)\ZeroTier\One\zerotier-cli.bat";
+Start-Process -FilePath $destination -ArgumentList "join 9f77fc393e977c16" -Wait
+$exitCode = $LASTEXITCODE
+Write-Verbose "Exit code was $exitCode"
+$validExitCodes = @(0, 1605, 1614, 1641, 3010)
+if ($validExitCodes -contains $exitCode) {
+Exit 0
+}
+Exit $exitCode
