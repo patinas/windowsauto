90ca7b0125c4afc76dc8a98eed987c74c715a180
patinas <andreas.patinas@gmail.com>
2022-11-05 10:26:27 +0000

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -5,7 +5,8 @@ choco install tailscale googlechrome 7zip vlc adobereader winrar googledrive sha
 
 choco feature enable -y allowGlobalConfirmation
 
+tailscale up --authkey tskey-auth-kxhmxL5CNTRL-LYB5qBFrzJXkNSFmh9HmRXQN4GGiKLVRJ --unattended
+
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
-tailscale up --authkey tskey-auth-kxhmxL5CNTRL-LYB5qBFrzJXkNSFmh9HmRXQN4GGiKLVRJ --unattended
