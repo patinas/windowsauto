08b9d989866e8b71a251591207daee3e05001910
patinas <andreas.patinas@gmail.com>
2022-11-07 22:05:21 +0000

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -1,7 +1,7 @@
 # Run as Administrator
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install tailscale googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey powertoys mailspring brave putty microsoft-office-deployment tabby etcher -y
+choco install tailscale googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey powertoys mailspring brave putty docker-desktop tabby etcher -y
 
 choco feature enable -y allowGlobalConfirmation
 
