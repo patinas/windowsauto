f34fbdf869135915ad3414caf07fc9cefdba269c
patinas <andreas.patinas@gmail.com>
2022-11-09 20:55:09 +0100

    Update parsec.bat

--- parsec.bat
+++ parsec.bat
@@ -1,5 +1,3 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-choco install parsec -y
-
-choco feature enable -y allowGlobalConfirmation
+powershell.exe -ExecutionPolicy Bypass -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
+powershell.exe -ExecutionPolicy Bypass -Command "choco feature enable -y allowGlobalConfirmation"
+powershell.exe -ExecutionPolicy Bypass -Command "choco install parsec -y"
