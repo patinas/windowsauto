c056603d37a3dd4bb5b4082bafd009971d836d04
patinas <andreas.patinas@gmail.com>
2022-11-13 21:51:07 +0100

    Delete EXECONVERTER.ps1

--- EXECONVERTER.ps1
+++
@@ -1,7 +0,0 @@
-﻿cd 'G:\Shared drives\DATA\Downloads\WinApps\WindowsAuto'
-
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-Install-Module ps2exe
-Import-Module ps2exe
-Invoke-ps2exe .\WindowsAuto.ps1 .\WindowsAuto.exe
