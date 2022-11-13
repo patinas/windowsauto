c22307ec351a499b2ed4c62b7567631010454181
patinas <andreas.patinas@gmail.com>
2022-11-13 21:43:54 +0100

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -1,4 +1,3 @@
-# Run as Administrator
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install tailscale googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey powertoys mailspring brave putty tabby etcher parsec -y
