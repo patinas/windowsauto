f086448c05dabae547c9fe2942603b1f6aae008b
patinas <andreas.patinas@gmail.com>
2022-11-21 09:22:56 +0100

    Update WindowsAuto.ps1

--- WindowsAuto.ps1
+++ WindowsAuto.ps1
@@ -7,6 +7,8 @@ choco feature enable -y allowGlobalConfirmation
 # tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
 zerotier-cli join 0cccb752f77190cf
 
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
+
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
