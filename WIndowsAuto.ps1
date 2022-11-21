e837390cad0ec53337628257c4b6aaf5c4907b4b
patinas <andreas.patinas@gmail.com>
2022-11-21 09:02:28 +0100

    Update WIndowsAuto.ps1

--- WIndowsAuto.ps1
+++ WIndowsAuto.ps1
@@ -1,10 +1,11 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install tailscale googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey powertoys mailspring brave putty tabby etcher parsec -y
+choco install tailscale zerotier-one googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey powertoys mailspring brave putty tabby etcher parsec -y
 
 choco feature enable -y allowGlobalConfirmation
 
-tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
+# tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
+zerotier-cli join 0cccb752f77190cf
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
