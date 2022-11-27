83115e0b28a67e335571110958fd20096845874c
patinas <andreas.patinas@gmail.com>
2022-11-27 18:06:03 +0100

    Update WindowsAuto.ps1

--- WindowsAuto.ps1
+++ WindowsAuto.ps1
@@ -1,6 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install snappy-driver-installer-origin tailscale zerotier-one chrome-remote-desktop-chrome chrome-remote-desktop-host microsoft-teams googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey brave putty tabby etcher -y
+choco install honeyview snappy-driver-installer-origin tailscale zerotier-one chrome-remote-desktop-chrome chrome-remote-desktop-host microsoft-teams googlechrome 7zip vlc adobereader winrar googledrive sharex obsidian pandoc autohotkey brave putty tabby etcher -y
 choco upgrade all -y
 choco feature enable -y allowGlobalConfirmation
 
