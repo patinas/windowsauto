53a3f328563aca63224d8aab5334bbc3e198e496
patinas <andreas.patinas@gmail.com>
2022-12-01 10:13:13 +0100

    Update tailscale.ps1

--- tailscale.ps1
+++ tailscale.ps1
@@ -2,4 +2,3 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 choco install tailscale -y
 tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
-tailscale up --advertise-routes=192.168.3.0/24
