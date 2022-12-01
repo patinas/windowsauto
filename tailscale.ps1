1c42af10f16412dd1e0705ef026a886a61b45ed9
patinas <andreas.patinas@gmail.com>
2022-12-01 10:01:14 +0100

    Update tailscale.ps1

--- tailscale.ps1
+++ tailscale.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 choco install tailscale -y
 tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
 tailscale up --advertise-routes=192.168.3.0/24
