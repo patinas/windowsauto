bc785d9784c74c89e82c869822997c8af9ea7344
patinas <andreas.patinas@gmail.com>
2022-12-01 15:29:44 +0100

    Update tailscale.ps1

--- tailscale.ps1
+++ tailscale.ps1
@@ -1,14 +1,12 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install tailscale zerotier-one -y
-choco upgrade all -y
-choco feature enable -y allowGlobalConfirmation
-
-tailscale up --authkey tskey-auth-kmYWEs4CNTRL-E58dqRPnpjitN4wH1JYshiDnogxNZCqR --unattended
-
-zerotier-cli join 0cccb752f77190cf
-
 netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow
 
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+choco install tailscale zerotier-one -y
+choco upgrade all -y
+choco feature enable -y allowGlobalConfirmation
+
+
