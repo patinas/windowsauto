1ba2d41950933c6e2069e97e463be7dc89805019
patinas <andreas.patinas@gmail.com>
2023-08-07 15:44:58 +0200

    v4

---
+++ SystemWizard.bat
@@ -0,0 +1,32 @@
+@echo off
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+::ENTER YOUR CODE BELOW::   
+
+
+
+powershell.exe -ExecutionPolicy Bypass -Command "irm https://raw.githubusercontent.com/patinas/windowsauto/main/SystemWizard.ps1 | iex"
+
+
+
+
+::END OF YOUR CODE::
+
+echo.
+echo...Script Complete....
+echo.
+
+
+
+
