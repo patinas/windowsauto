1696b82f2bf0d42306038ef3c94c4a29cbc72787
patinas <andreas.patinas@gmail.com>
2023-08-07 15:43:52 +0200

    v3

--- SystemWizard.ps1
+++ SystemWizard.ps1
@@ -105,7 +105,7 @@ Stop-Process -Name "Explorer"
 
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install googlechrome nvidia-geforce-now -y
+choco install googlechrome -y
 
 # Check if Google Chrome is installed
 $chromePath = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe').'(Default)'
